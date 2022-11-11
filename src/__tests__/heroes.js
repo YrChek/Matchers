a6b0ae56fd85b28d3e5d1c09036d2de5936a37b1
YrChek <y.chekmaev@mail.ru>
2022-11-11 18:46:47 +0300

    Первый коммит

---
+++ src/__tests__/heroes.js
@@ -0,0 +1,16 @@
+import sortingHealth from '../heroes';
+
+test('Correct sorting test', () => {
+  const names = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const sortedNames = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = sortingHealth(names);
+  expect(result).toEqual(sortedNames);
+});
---
+++ src/heroes.js
@@ -0,0 +1,3 @@
+export default function sortingHealth(array) {
+  return array.sort((a, b) => b.health - a.health);
+}
